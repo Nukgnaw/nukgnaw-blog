bca21e33eecb94c76a674e6f3f4339833b89c1fc
nukgnaw <Kun.wang@ayudasystems.com>
2021-01-31 11:51:59 -0500

    Add a domain name

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd blog/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.nukgnaw.com' > CNAME
 
 git init
 git config user.name "nukgnaw" 
